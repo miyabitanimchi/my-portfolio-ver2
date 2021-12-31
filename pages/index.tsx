034f11810e0b29215baa683227f42d818d094b69
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-12-31 13:52:08 -0800

    style hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,8 +38,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <section>
-          <div className={styles.titlesWrap}>
+        <section className={styles.heroSection}>
+          {/* <div className={styles.titlesWrap}>
             <h1 className={utilStyles.heading2Xl}>
               Hello, I am <span className={styles.name}>MIYABI TANIMICHI</span>
             </h1>
@@ -75,52 +75,16 @@ const Home: NextPage = () => {
               <SiFigma />
               <SiHeroku />
             </div>
+          </div> */}
+          <div className={styles.heroNames}>
+            <h1 className={utilStyles.headingJumbo}>Miyabi</h1>
+            <h1 className={utilStyles.headingJumbo}>Tanimichi</h1>
           </div>
-
-          {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
+          <div className={styles.heroJobPosition}>
+            <h3 className={utilStyles.headingSubJumbo}>Front End Developer</h3>
+          </div>
         </section>
       </main>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
     </div>
   );
 };
