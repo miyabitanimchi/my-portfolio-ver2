877dab2013d9d6458f38216b505086e7a75ba5ef
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2022-01-05 19:01:40 -0800

    fix some style

--- components/layouts/heroSection/HeroSection.tsx
+++ components/layouts/heroSection/HeroSection.tsx
@@ -6,30 +6,32 @@ import Link from "next/link";
 const HeroSection = () => (
   <section className={styles.heroSection} id="home-section">
     <div className={styles.heroWrap}>
-      <h1
-        className={`${utilStyles.headingJumbo} ${styles.heroFName} ${utilStyles.gradientTextKit} ${utilStyles.gradientPinkBlue}`}
-      >
-        Miyabi
-      </h1>
-      <h1
-        className={`${utilStyles.headingJumbo} ${styles.heroLName} ${utilStyles.gradientTextKit} ${utilStyles.gradientPinkBlue}`}
-      >
-        Tanimichi
-      </h1>
-      <div className={styles.jobPositionWrap}>
-        <h3
-          className={`${utilStyles.headingSecJumbo} ${styles.jobPosition} ${utilStyles.gradientTextKit} ${utilStyles.gradientGray}`}
+      <div className={styles.titleWrap}>
+        <h1
+          className={`${utilStyles.headingJumbo} ${styles.heroFName} ${utilStyles.gradientTextKit} ${utilStyles.gradientPinkBlue}`}
         >
-          Front End Developer
-        </h3>
+          Miyabi
+        </h1>
+        <h1
+          className={`${utilStyles.headingJumbo} ${styles.heroLName} ${utilStyles.gradientTextKit} ${utilStyles.gradientPinkBlue}`}
+        >
+          Tanimichi
+        </h1>
+        <div className={styles.jobPositionWrap}>
+          <h3
+            className={`${utilStyles.headingSecJumbo} ${styles.jobPosition} ${utilStyles.gradientTextKit} ${utilStyles.gradientGray}`}
+          >
+            Front End Developer
+          </h3>
+        </div>
       </div>
+      <div className={styles.test}></div>
+      <Link href="#about">
+        <a className={styles.arrowWrap}>
+          <span className={styles.arrow}></span>
+        </a>
+      </Link>
     </div>
-    <div className={styles.test}></div>
-    <Link href="#about">
-      <a className={styles.arrowWrap}>
-        <span className={styles.arrow}></span>
-      </a>
-    </Link>
   </section>
 );
 
